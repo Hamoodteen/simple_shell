ef10f2b2bbd48d1a0bb0ece7245dfa4e980d77b5
Hamoodteen <ahmed104567@outlook.com>
2023-09-10 02:30:05 +0300

    0

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
+#include "main.h"
 #include "shell.h"
 /**
  * main - func
  * @argc: int
- * @argv: arguments
+ * @argv: args
  * Return: 0 or 1
 */
 int main(int argc, char **argv)
 {
+	(void)argc;
+	(void)argv;
 	return (0);
 }
