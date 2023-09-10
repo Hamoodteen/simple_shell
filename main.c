d9a4f9e6e8d748c606cf045a4d564684cd4b2e6d
Hamoodteen <ahmed104567@outlook.com>
2023-09-10 06:00:34 +0300

    0

--- main.c
+++ main.c
@@ -8,38 +8,63 @@
 int main(int argc, char *argv[])
 {
 	char *s;
-	char *path;
-	char *ev[1];
+	char *tok;
+	char *args[50];
 	size_t len;
+	pid_t child;
+	int i;
+	int a;
+	int status;
 
-	(void)ev;
-	ev[0] = "PATH=/bin:/usr/bin";
-	ev[1] = NULL;
-	path = "/usr/bin/";
+	for (a = 0; a < 50; a++)
+    	args[a] = NULL;
 	(void)argc;
 	(void)argv;
 	while (1)
 	{
-		len = 0;
+		len = status = i = 0;
 		s = NULL;
 		_puts("$: ");
 		if (getline(&s, &len, stdin) != -1)
 		{
-			if (strcmp(s, "exit\n") == 0)
+			s[strcspn(s, "\n")] = '\0';
+			tok = strtok(s, " ");
+			while (tok != NULL)
+			{
+				args[i] = tok;
+				tok = strtok(NULL, " ");
+				i++;
+			}
+			args[i] = NULL;
+			if (strcmp(s, "exit") == 0)
 			{
 				free(s);
 				exit(EXIT_SUCCESS);
 			}
-			if (execve(path, &s, ev) == -1)
-			{
-				perror("Error");
-			}
+			child = fork();
+			if (child == -1)
+            {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            if (child == 0)
+            {
+                if (execve(args[0], args, NULL) == -1)
+                {
+                    perror("execve");
+                    exit(EXIT_FAILURE);
+                }
+				exit(EXIT_SUCCESS);
+            }
+            else
+                waitpid(child, &status, 0);
 		}
 		else
 		{
 			perror("Error");
 			exit(EXIT_FAILURE);
 		}
+	free(s);
 	}
 	return (0);
 }
