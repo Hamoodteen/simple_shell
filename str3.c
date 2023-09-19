c3193eefe989b9170c8620892d88fa40dc6e28b0
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-09-20 01:00:47 +0300

    done

--- str3.c
+++ str3.c
@@ -1,6 +1,12 @@
 #include "shell.h"
 
-
+/**
+ * check_substring - check if substring exists in string
+ * @str: the string
+ * @substring: the substring
+ * Return: 1 if the substring exists in string
+ * or 0 if the substring doesn't exist in string
+*/
 int check_substring(char *str, char *substring)
 {
 	int i;
