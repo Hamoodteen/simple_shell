2768495b1270b5c85350a08887acce7803539c0b
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-09-20 23:13:43 +0300

    done

--- str3.c
+++ str3.c
@@ -2,7 +2,7 @@
 
 /**
  * check_substring - check if substring exists in string
- * @str: gthe string
+ * @str: theg string
  * @substring: the substring
  * Return: 1 if the substring exists in string
  * or 0 if the substring doesn't exist in string
