409c817aa01fe3ee4d2a3dfc51ab7cfd3c0d3791
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-09-20 23:13:00 +0300

    done

--- str3.c
+++ str3.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * check_substring - check if substgring exists in string
+ * check_substring - check if substring exists in string
  * @str: the string
  * @substring: the substring
  * Return: 1 if the substring exists in string
