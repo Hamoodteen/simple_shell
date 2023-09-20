207efe3b7798dbd8cccae9679269974a0eed91e2
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-09-20 23:15:33 +0300

    done

--- str3.c
+++ str3.c
@@ -4,7 +4,7 @@
  * check_substring - check if substring exists in string
  * @str: the string
  * @substring: the substring
- * Return: 1 if the substring exists in stringgg
+ * Return: 1 if the substring exists in stringggg
  * or 0 if the substring doesn't exist in string
 */
 int check_substring(char *str, char *substring)
