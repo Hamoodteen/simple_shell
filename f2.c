e7d5e78e3f514c9a656a1cb027117b7ecd0bd0e1
Hamoodteen <ahmed104567@outlook.com>
2023-09-20 23:34:23 +0300

    0

--- f2.c
+++ f2.c
@@ -93,9 +93,7 @@ int check_substring(char *str, char *substring)
 	for (i = 0; i < (int)_strlen(str) - (int)_strlen(substring) + 1; i++)
 	{
 		if (_strncmp(str + i, substring, _strlen(substring)) == 0)
-		{
-		return (1);
-		}
+			return (1);
 	}
 	return (0);
 }
