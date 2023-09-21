e8abf519a652774f1ba8bae7884732240924ee23
Hamoodteen <ahmed104567@outlook.com>
2023-09-21 03:25:04 +0300

    0

--- forks.c
+++ forks.c
@@ -41,11 +41,14 @@ int myexitenvcd(char *s, char *args[], char *env[], char *argv0, int cnt)
 		return (0); }
 	else if (_strcmp(s, "cd") == 0)
 	{
-		_cd(args, argv0, cnt);
+		if (_strcmp(_cd(args, argv0, cnt), _getenv("HOME")) == 0)
+			return (0);
+		else
+			return (2);
 		(void)mycd;
 		/* mycd = _cd(args, argv0, cnt); */
 		/* write(STDIN_FILENO, mycd, _strlen(mycd)); */
-		return (-1); }
+	}
 	return (1); }
 
 /**
